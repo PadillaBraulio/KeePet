2d970cac4fd4e2314aae315a8de9afc0f1c71a04
Braulio <brauliopadilla@gmail.com>
2016-05-11 23:23:08 -0600

    setting default ubication for the Camera

--- KeePet/app/src/main/java/com/example/root/keepet/MainActivity.java
+++ KeePet/app/src/main/java/com/example/root/keepet/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.root.keepet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private static final LatLng HOME = new LatLng(14.5411093, -90.4260691);
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HOME, 16));
     }
 }
